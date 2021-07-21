db59bd7e43733353749a3e332fb8c3e4c55dc126
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:15:51 +0300

    Adding PagingDataResult

---
+++ OrderApi/src/OrderApi/Data/PagingDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using OrderApi.Data.Entities;
+
+namespace OrderApi.Data
+{
+    public class PagingDataResult
+    {
+        public IEnumerable<OrderEntity> Orders { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
