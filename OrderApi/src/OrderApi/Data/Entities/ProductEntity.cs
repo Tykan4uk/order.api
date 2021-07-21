e11bc19b0ec0bd373e2af8db353ee6ad04c4e880
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 15:21:03 +0300

    Adding ProductEntity

---
+++ OrderApi/src/OrderApi/Data/Entities/ProductEntity.cs
@@ -0,0 +1,16 @@
+﻿using OrderApi.Common.Enums;
+
+namespace OrderApi.Data.Entities
+{
+    public class ProductEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+
+        public string OrderId { get; set; }
+        public OrderEntity Order { get; set; }
+    }
+}
