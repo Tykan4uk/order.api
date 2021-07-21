0eaf13918a04ee17b6fcb4f900150e7e9d97870c
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 15:39:22 +0300

    Adding ProductModel

---
+++ OrderApi/src/OrderApi/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using OrderApi.Common.Enums;
+
+namespace OrderApi.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+    }
+}
