88b05cba80cceaec5fa13b15387e8de97a6a88ed
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:16:00 +0300

    Adding IOrderProvider

---
+++ OrderApi/src/OrderApi/DataProviders/Abstractions/IOrderProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrderApi.Data;
+using OrderApi.Data.Entities;
+
+namespace OrderApi.DataProviders.Abstractions
+{
+    public interface IOrderProvider
+    {
+        Task<PagingDataResult> GetByPageAsync(string userId, int page, int pageSize);
+        Task<List<ProductEntity>> GetByIdAsync(string userId, string orderId);
+        Task<OrderEntity> AddAsync(string userId, List<ProductEntity> products);
+    }
+}
