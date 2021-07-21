e8a7b9e87aff40ad249fbb80d8de2ae75b76d253
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:29:34 +0300

    GetByIdResponse: adding property Products

--- OrderApi/src/OrderApi/Models/Responses/GetByIdResponse.cs
+++ OrderApi/src/OrderApi/Models/Responses/GetByIdResponse.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OrderApi.Models.Responses
 {
     public class GetByIdResponse
     {
+        public List<ProductModel> Products { get; set; }
     }
 }
