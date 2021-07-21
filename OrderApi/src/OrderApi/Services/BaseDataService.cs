58966061763c99604bba815fe2085266a05720c3
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 13:40:01 +0300

    Adding class-wrap BaseDataService

---
+++ OrderApi/src/OrderApi/Services/BaseDataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderApi.Data;
+
+namespace OrderApi.Services
+{
+    public class BaseDataService
+    {
+        private readonly OrdersDbContext _ordersDbContext;
+
+        public BaseDataService(IDbContextFactory<OrdersDbContext> dbContextFactory)
+        {
+            _ordersDbContext = dbContextFactory.CreateDbContext();
+        }
+
+        protected async Task<T> ExecuteSafe<T>(Func<Task<T>> action)
+        {
+            using (var transaction = _ordersDbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = await action();
+                    transaction.Commit();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
