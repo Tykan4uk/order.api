eba8453f2919a7be8bad3de70b31c38d06132c49
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:20:03 +0300

    Adding OrderModel

---
+++ OrderApi/src/OrderApi/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OrderApi.Models
+{
+    public class OrderModel
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
