a98a8330b28c0304490caf8b96c995eaf1beda16
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:21:02 +0300

    Adding MappingProfile

---
+++ OrderApi/src/OrderApi/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OrderApi.Data.Entities;
+using OrderApi.Models;
+
+namespace OrderApi.Common.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<OrderModel, OrderEntity>().ReverseMap();
+            CreateMap<ProductModel, ProductEntity>().ReverseMap();
+        }
+    }
+}
