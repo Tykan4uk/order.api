5ff9cae39abc4420bee24edaab3495522dd24f81
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 15:39:33 +0300

    Adding IOrderService

---
+++ OrderApi/src/OrderApi/Services/Abstractions/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrderApi.Models;
+using OrderApi.Models.Responses;
+
+namespace OrderApi.Services.Abstractions
+{
+    public interface IOrderService
+    {
+        Task<GetByPageResponse> GetByPageAsync(string userId, int page, int pageSize);
+        Task<GetByIdResponse> GetByIdAsync(string userId, string orderId);
+        Task<AddResponse> AddAsync(string userId, List<ProductModel> products);
+    }
+}
