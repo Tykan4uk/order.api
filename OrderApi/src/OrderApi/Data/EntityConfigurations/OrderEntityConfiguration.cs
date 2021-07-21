d34ebebd04621ea62985c1458c9c9079faa79de0
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 15:21:53 +0300

    Adding OrderEntityConfiguration and ProductEntityConfiguration

---
+++ OrderApi/src/OrderApi/Data/EntityConfigurations/OrderEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderApi.Data.Entities;
+
+namespace OrderApi.Data.EntityConfigurations
+{
+    public class OrderEntityConfiguration : IEntityTypeConfiguration<OrderEntity>
+    {
+        public void Configure(EntityTypeBuilder<OrderEntity> builder)
+        {
+            builder.ToTable("Order")
+                .HasKey(h => h.Id);
+
+            builder.Property(p => p.CreateDate)
+                .IsRequired()
+                .HasColumnName("CreateDate");
+        }
+    }
+}
---
+++ OrderApi/src/OrderApi/Data/EntityConfigurations/ProductEntityConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderApi.Data.Entities;
+
+namespace OrderApi.Data.EntityConfigurations
+{
+    public class ProductEntityConfiguration : IEntityTypeConfiguration<ProductEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductEntity> builder)
+        {
+            builder.ToTable("Product")
+                .HasKey(h => h.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasColumnName("Description")
+                .HasMaxLength(255);
+
+            builder.Property(p => p.Price)
+                .IsRequired()
+                .HasColumnName("Price");
+
+            builder.Property(p => p.Type)
+                .IsRequired()
+                .HasColumnName("ProductType");
+
+            builder.HasOne(h => h.Order)
+                .WithMany(w => w.Products)
+                .HasForeignKey(h => h.OrderId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
