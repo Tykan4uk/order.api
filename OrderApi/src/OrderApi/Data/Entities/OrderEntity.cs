cbbeb3f6911560ca39eea6288afc6068ae749148
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 13:22:41 +0300

    Adding OrderEntity

---
+++ OrderApi/src/OrderApi/Data/Entities/OrderEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OrderApi.Common.Enums;
+
+namespace OrderApi.Data.Entities
+{
+    public class OrderEntity
+    {
+        public string OrderId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+    }
+}
