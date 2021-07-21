39758d6f4d3069e3dd632a9007628eed5ec57070
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 13:22:59 +0300

    Adding OrdersDbContext

---
+++ OrderApi/src/OrderApi/Data/OrdersDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Data.Entities;
+
+namespace OrderApi.Data
+{
+    public class OrdersDbContext : DbContext
+    {
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OrderEntity> Orders { get; set; } = null!;
+    }
+}
