c3df86ebc9c3481e6a0ef60dc6d982f6be5de66b
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:38:43 +0300

    Adding ManageController and OrderBffController

---
+++ OrderApi/src/OrderApi/Controllers/ManageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderApi.Models.Requests;
+using OrderApi.Services.Abstractions;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class ManageController : ControllerBase
+    {
+        private readonly ILogger<ManageController> _logger;
+        private readonly IOrderService _orderService;
+
+        public ManageController(
+            ILogger<ManageController> logger,
+            IOrderService orderService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByPage([FromQuery] GetByPageRequest request)
+        {
+            var result = await _orderService.GetByPageAsync(request.UserId, request.Page, request.PageSize);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById([FromQuery] GetByIdRequest request)
+        {
+            var result = await _orderService.GetByIdAsync(request.UserId, request.OrderId);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddRequest request)
+        {
+            var result = await _orderService.AddAsync(request.UserId, request.Products);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+    }
+}
---
+++ OrderApi/src/OrderApi/Controllers/OrderBffController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderApi.Models.Requests;
+using OrderApi.Services.Abstractions;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class OrderBffController : ControllerBase
+    {
+        private readonly ILogger<OrderBffController> _logger;
+        private readonly IOrderService _orderService;
+
+        public OrderBffController(
+            ILogger<OrderBffController> logger,
+            IOrderService orderService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetByPage([FromBody] GetByPageRequest request)
+        {
+            var result = await _orderService.GetByPageAsync(request.UserId, request.Page, request.PageSize);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetById([FromBody] GetByIdRequest request)
+        {
+            var result = await _orderService.GetByIdAsync(request.UserId, request.OrderId);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddRequest request)
+        {
+            var result = await _orderService.AddAsync(request.UserId, request.Products);
+            return result != null ? Ok(result) : BadRequest(result);
+        }
+    }
+}
