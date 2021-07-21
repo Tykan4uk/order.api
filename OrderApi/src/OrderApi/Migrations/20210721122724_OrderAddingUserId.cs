d68bbcfa9c88dcc991ed99dda701f8f5d2e5efe7
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 15:29:22 +0300

    Adding migration OrderAddingUserId

---
+++ OrderApi/src/OrderApi/Migrations/20210721122724_OrderAddingUserId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderApi.Migrations
+{
+    public partial class OrderAddingUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Order",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Order");
+        }
+    }
+}
