da3f5a87fbaba3383e039682b14323d9730c1258
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-21 16:26:42 +0300

    GetByPageResponse: adding properties

--- OrderApi/src/OrderApi/Models/Responses/GetByPageResponse.cs
+++ OrderApi/src/OrderApi/Models/Responses/GetByPageResponse.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OrderApi.Models.Responses
 {
     public class GetByPageResponse
     {
+        public IEnumerable<OrderModel> Orders { get; set; }
+        public int TotalRecords { get; set; }
     }
 }
