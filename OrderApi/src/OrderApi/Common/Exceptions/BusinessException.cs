5b7ae27fe7e2a2784d174a3c88bc7f554cbf3cf9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 22:33:28 +0300

    Adding BusinessException

---
+++ OrderApi/src/OrderApi/Common/Exceptions/BusinessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OrderApi.Common.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message)
+        {
+            Message = message;
+        }
+
+        public override string Message { get; }
+    }
+}
