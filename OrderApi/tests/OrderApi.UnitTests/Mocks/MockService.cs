5cc20caaaf5f2d427d860e50711872e29c1cd2fe
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-24 22:40:30 +0300

    Adding MockService

---
+++ OrderApi/tests/OrderApi.UnitTests/Mocks/MockService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OrderApi.Data;
+using OrderApi.Services;
+using OrderApi.Services.Abstractions;
+
+namespace OrderApi.UnitTests.Mocks
+{
+    public class MockService : BaseDataService<OrdersDbContext>
+    {
+        public MockService(
+            IDbContextWrapper<OrdersDbContext> dbContextWrapper,
+            ILogger<MockService> logger)
+            : base(dbContextWrapper, logger)
+        {
+        }
+
+        public async Task RunWithException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                throw new Exception();
+            });
+        }
+
+        public async Task RunWithoutException()
+        {
+            await ExecuteSafe<bool>(() =>
+            {
+                return Task.FromResult(true);
+            });
+        }
+    }
+}
